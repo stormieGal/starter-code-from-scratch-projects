f465db1457f9a1bf99cd8badef7ff57325b65046
StormieGal <tfeujio@gmail.com>
2020-10-02 18:42:41 +0100

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,5 @@
+/**
+ * @abstract - A script file
+ * @author- stormieGal
+ * @description - This file modifies index.html file
+ */
---
+++ utilities.js
@@ -0,0 +1,6 @@
+/**
+ * @abstract - A collection of utility function
+ * @author- stormieGal
+ * @description - This file modifies contains utilities function that can be use in any JS file.
+ * This file avoid me to import CDN or heavy libraries just to use on function.
+ */
